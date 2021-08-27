d0da7fe4f63bdaa4ecd95f3b1901aefdd6b6cc67
flaviocampos <flaviorcampos15@gmail.com>
2021-08-27 16:58:12 -0300

    Iniciando projeto API

---
+++ src/main/java/com/adesao/fundacao/FundacaoApplication.java
@@ -0,0 +1,13 @@
+package com.adesao.fundacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adesao/fundacao/FundacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adesao.fundacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
